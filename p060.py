55dcce24e91ad2b8d06b97128b4324e93a4f6787
goby <goby@foxmail.com>
2016-11-04 21:38:43 +0800

    Rename

--- p060.py
+++ p060.py
@@ -1,43 +1,66 @@
-########################################
-# The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes 
-#   and concatenating them in any order the result will always be prime. For 
-#   example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these 
-#   four primes, 792, represents the lowest sum for a set of four primes with 
-#   this property.
-# 
-# Find the lowest sum for a set of five primes for which any two primes 
-#   concatenate to produce another prime.
-########################################
+# Problem 60
+# Prime pair sets
+# Use dfs to get result. It is slowly. Running 144s in my laptop
 
 def isprime(n):
-    if n <=1: return False
+    if n <= 1: return False
     if n == 2: return True
     if n % 2 == 0: return False
     if n < 9: return True
     if n % 3 == 0: return False
-    counter = 5
-    while counter * counter <= n:
-        if n % counter == 0 or n % (counter + 2) == 0: return False
-        counter += 6
-
-    return True
+    count = 5
+    while count * count <= n:
+        if n % count == 0 or n % (count + 2) == 0: return False
+        count = count + 6
     
-def validnum(n):
-    n = str(n)
-    if sum([int(x) for x in n]) % 3 != 1: return False
-    for src in ['7','109', '673', '3']:
-        if not (isprime(int(n)) and isprime(int(src+n)) and isprime(int(n+src))):
-            return False
-        if src == '673': print n
     return True
-    
-def getfiveprime():
-    x = 677
-    while True:
-        if validnum(x):
-            return 3+7+109+673+x
-        x += 2
 
-if __name__ == '__main__':
-    print getfiveprime()
+def valid(n,x):
+    return isprime(x) and isprime(int(str(x)+str(n))) and  isprime(int(str(n)+str(x)))
+    
+def getprimepair(n):
+    ret = []
+    for i in range(3, 10000):
+        if valid(n, i):
+            ret.append(i)
             
+    return ret
+    
+def getallpair():
+    all = {}
+    for i in range(3, 10000):
+        if isprime(i):
+            all[i] = getprimepair(i)
+    
+    return all
+
+def dfs(pair, li):
+    current = li[-1]
+    level = len(li)
+    for i in li[:-1]: 
+        if not (i in pair[current]): return 0;
+        
+    if (level == 5):
+        print li
+        return sum(li)
+        
+    min = 9999999
+    if level < 5:
+        for i in pair[current]:
+            if i in li or i < current: continue
+            r = dfs(pair, li+[i])
+            if r and r < min:
+                min = r
+        if min != 9999999:
+            return min
+    return 0 # not found
+    
+if __name__ == '__main__':
+    pair = getallpair()
+    min = 9999999
+    for i in pair:
+        r = dfs(pair, [i])
+        if r and r < min:
+            min = r
+    
+    print min
